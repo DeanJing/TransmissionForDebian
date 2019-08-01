10bbe09c49437232b34cf84cc24776e20b38c1b1
DeanJing <13661610815@126.com>
2019-08-01 23:10:36 +0800

    Update TransmissionForDebian.sh

--- TransmissionForDebian.sh
+++ TransmissionForDebian.sh
@@ -38,20 +38,19 @@ read -p "Set port(1989):" port
 if [ "$port" = "" ]; then
 	port="1989"
 fi
-
-	get_char()
-	{
-	SAVEDSTTY=`stty -g`
-	stty -echo
-	stty cbreak
-	dd if=/dev/tty bs=1 count=1 2> /dev/null
-	stty -raw
-	stty echo
-	stty $SAVEDSTTY
-	}
-	echo ""
-	echo "Press any key to start...or Press Ctrl+c to cancel"
-	char=`get_char`
+    get_char()
+    {
+    SAVEDSTTY=`stty -g`
+    stty -echo
+    stty cbreak
+    dd if=/dev/tty bs=1 count=1 2> /dev/null
+    stty -raw
+    stty echo
+    stty $SAVEDSTTY
+    }
+    echo ""
+    echo "Press any key to start...or Press Ctrl+c to cancel"
+    char=`get_char`
 
 # START
 apt-get update
